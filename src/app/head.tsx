9c5a93d1baf6076ad0684f124ed256120e4904dd
Brixter Porras <brixterporras@gmail.com>
2023-02-20 14:55:56 +0800

    first commit

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <title>Brix</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Brixter Porras, Web Developer in Philippines. Specialities: React, Typescript, Javascript, Node/Express Js."
+      />
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="title" content="Brixter Porras" />
+      <meta name="image" content="/og-image.png" />
+    </>
+  );
+}
