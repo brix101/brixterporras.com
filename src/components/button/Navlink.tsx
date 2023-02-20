dd19771fe47c7166339c24e2d2c4f7aae3e86d0e
Brixter Porras <brixterporras@gmail.com>
2023-02-20 15:19:48 +0800

    feat: initialize tailwind

---
+++ src/components/button/Navlink.tsx
@@ -0,0 +1,14 @@
+function Navlink() {
+  return (
+    <button
+      type="button"
+      className="mr-2 mb-2 rounded-lg border border-gray-200 bg-white py-2.5 px-5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+    >
+      <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-gray-900">
+        Cyan to blue
+      </span>
+    </button>
+  );
+}
+
+export default Navlink;
