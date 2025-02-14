324b8cc5b2d52de12e0d2cbf00ea4afc8f5ada86
Brixter Porras <60214966+Brix101@users.noreply.github.com>
2025-02-14 17:33:03 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <TailwindIndicator />
         </ThemeProvider>
 
-        <Analytics />
+{/*         <Analytics /> */}
       </body>
     </html>
   )
